61a9adf40fcdf64d769388f0d5f8c0b596c5df99
crane <je.pavel@seznam.cz>
2026-08-21 01:56:41 +0200

    created commands struct + some methods)

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,8 @@ package config
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -11,6 +13,45 @@ const configFileName = ".gatorconfig.json"
 type Config struct {
 	DBURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
+}
+
+type state struct {
+	config *Config
+}
+
+type command struct {
+	name string
+	args []string
+}
+
+type commands struct {
+	commands map[string]func(s *state, c command) error
+}
+
+func (cmds *commands) run(s *state, c command) error {
+	function, ok := cmds.commands[c.name]
+	if !ok {
+		return errors.New("command doesnt exist")
+	}
+	err := function(s, c)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cmds *commands) register(name string, f func(*state, command) error) {
+	cmds.commands[name] = f
+}
+
+func handlerLogin(s *state, c command) error {
+	if len(c.args) == 0 {
+		return errors.New("login name missing")
+	} else {
+		s.config.CurrentUserName = c.args[0]
+		fmt.Println("user has been set")
+		return nil
+	}
 }
 
 func Read() (Config, error) {
