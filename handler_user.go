4c22c6aba389c67383a246cf9240ecf1ba5ade8e
crane <je.pavel@seznam.cz>
2026-08-21 04:02:49 +0200

    finished 3rd lesson

--- handler_user.go
+++ handler_user.go
@@ -10,6 +10,7 @@ func handlerLogin(s *state, c command) error {
 		return errors.New("login name missing")
 	} else {
 		s.config.CurrentUserName = c.args[0]
+		s.config.SetUser(s.config.CurrentUserName)
 		fmt.Println("user has been set")
 		return nil
 	}
